81416881933376d3419c4ad6613d659dc00e1746
alumen2101 <roadtoimmortal2101@gmail.com>
2025-01-26 01:23:47 +0330

    page router

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,14 @@
-import type { Config } from "tailwindcss";
-const flowbite = require("flowbite-react/tailwind");
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    flowbite.content(),
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
     },
   },
-  plugins: [
-    flowbite.plugin(),
-
-  ],
+  plugins: [],
 } satisfies Config;
